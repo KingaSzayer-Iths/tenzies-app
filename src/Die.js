fba3c3c4d00fdff0cb6c22f68492ce6c4d82dcd8
KingaSzayer-Iths <kinga.szayer@iths.se>
2024-01-21 09:54:04 +0100

    add new Die color

--- src/Die.js
+++ src/Die.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "rgb(249 136 203)" : "white"
     }
     return (
         <div className="die-face" style={styles} onClick={props.holdDice}>
